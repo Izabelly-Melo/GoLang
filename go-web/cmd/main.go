5d823c9f156d4a1fb5d39a76a202bc1b721c8941
izabelly melo <izabelly.dmelo@mercadolivre.com>
2024-12-10 00:07:45 -0300

    add configs middleware

--- go-web/cmd/main.go
+++ go-web/cmd/main.go
@@ -1,22 +1,35 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/izabelly/go-web/internal/handler"
+	"github.com/izabelly/go-web/internal/middlewares"
 	"github.com/izabelly/go-web/internal/service"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	rt := chi.NewRouter()
-
+	rt.Use(middleware.Logger)
+	rt.Use(middleware.Recoverer)
+	
 	// carrega produtos
 	service := service.NewServiceProducts("products.json")
 	handler := handler.NewProductHandler(service)
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Falha ao carregar as varáveis da .env")
+		return
+	}
+
 	rt.Get("/ping", handler.Ping)
 	rt.Route("/products", func(rt chi.Router) {
+		rt.Use(middlewares.Auth)
 		rt.Get("/", handler.GetAllProducts)
 		rt.Get("/{id}", handler.GetProductByID)
 		rt.Get("/search", handler.SearchProduct)
